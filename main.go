53c70e9a822d320093c62004804203410713e320
xcd0 <xcd0@xcd0.com>
2023-03-14 13:44:37 +0900

    標準入力からの処理に対応 引数なしの時標準入力からxmlを得る

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,24 +16,58 @@ import (
 func main() {
 
 	flag.Parse()
-	for i := 0; i < flag.NArg(); i++ {
-		name := flag.Arg(i)
-		file, err := os.Open(name) // *os.File
-		defer file.Close()
+
+	if flag.NArg() == 0 {
+		b, err := readXml(bufio.NewReader(os.Stdin))
 		if err != nil {
-			log.Println(err)
-			continue
+			log.Fatal(err)
 		}
-		r := bufio.NewReader(file)
-		jsonbb, err := xj.Convert(r)
-		var outb bytes.Buffer
-		json.Indent(&outb, jsonbb.Bytes(), "", "\t")
-		output, err := os.Create(filepath.Join(filepath.Dir(name), filepath.Base(name)+".json"))
-		defer output.Close()
-		if err != nil {
-			log.Println(err)
-			continue
+		if err := write(b, os.Stdout); err != nil {
+			log.Fatal(err)
 		}
-		outb.WriteTo(output)
+	} else {
+		for i := 0; i < flag.NArg(); i++ {
+			name := flag.Arg(i)
+			file, err := os.Open(name)
+			defer file.Close()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			output, err := os.Create(filepath.Join(filepath.Dir(name), filepath.Base(name)+".json"))
+			defer output.Close()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			b, err := readXml(bufio.NewReader(file))
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if err := write(b, output); err != nil {
+				log.Println(err)
+			}
+		}
 	}
+}
+
+func readXml(r io.Reader) (bytes.Buffer, error) {
+	jsonbb, err := xj.Convert(r)
+	if err != nil {
+		return bytes.Buffer{}, err
+	}
+	var out bytes.Buffer
+	json.Indent(&out, jsonbb.Bytes(), "", "\t")
+	return out, nil
+}
+
+func write(data bytes.Buffer, w io.Writer) error {
+	writer := bufio.NewWriter(w)
+	defer writer.Flush()
+	if _, err := data.WriteTo(writer); err != nil {
+		return err
+	}
+	return nil
 }
